d10f9f71d806634dc699f84353caf4cebdd6839b
Antonio Jesus <ajesus.etsii@gmail.com>
2012-03-18 13:09:15 +0100

    Creando entidad Usuario e Inmueble

--- src/Vecinos/InmuebleBundle/Entity/Inmueble.php
+++ src/Vecinos/InmuebleBundle/Entity/Inmueble.php
@@ -2,6 +2,7 @@
 
 namespace Vecinos\InmuebleBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -21,6 +22,91 @@ class Inmueble
      */
     private $id;
 
+    /**
+     * @var integer $num_personas
+     *
+     * @ORM\Column(name="num_personas", type="integer")
+     */
+    private $num_personas;
+
+    /**
+     * @var boolean $ocupado
+     *
+     * @ORM\Column(name="ocupado", type="boolean")
+     */
+    private $ocupado;
+
+    /**
+     * @var string $via
+     *
+     * @ORM\Column(name="via", type="string", length=100)
+     */
+    private $via;
+    
+    /**
+     * @var string $nombre_via
+     *
+     * @ORM\Column(name="nombre_via", type="string", length=255)
+     */
+    private $nombre_via;
+
+    /**
+     * @var integer $numero
+     *
+     * @ORM\Column(name="numero", type="integer")
+     */
+    private $numero;
+
+    /**
+     * @var integer $bloque
+     *
+     * @ORM\Column(name="bloque", type="integer")
+     */
+    private $bloque;
+
+    /**
+     * @var string $puerta
+     *
+     * @ORM\Column(name="puerta", type="string", length=1)
+     */
+    private $puerta;
+
+    /**
+     * @var integer $planta
+     *
+     * @ORM\Column(name="planta", type="integer")
+     */
+    private $planta;
+
+    /**
+     * @var string $nombre_propietario
+     *
+     * @ORM\Column(name="nombre_propietario", type="string", length=255)
+     */
+    private $nombre_propietario;
+
+    /**
+     * @var integer $habitaciones
+     *
+     * @ORM\Column(name="habitaciones", type="integer")
+     */
+    private $habitaciones;
+
+    /**
+     * 
+     *
+     * @ORM\ManyToMany(targetEntity="Usuario", inversedBy="inmuebles", cascade={"persist"})
+     * @ORM\JoinTable(name="inmueble_usuario",
+     *		joinColumns={@ORM\JoinColumn(name="inmueble_id", referencedColumnName="id")},
+     *		inverseJoinColumns={@ORM\JoinColumn(name="usuario_id", referencedColumnName="id")}
+     *		)
+     */
+    private $usuarios;
+
+    public function __construct()
+    {
+        $this->usuarios = new ArrayCollection();
+    }
 
     /**
      * Get id
@@ -31,4 +117,226 @@ class Inmueble
     {
         return $this->id;
     }
+
+    /**
+     * Set num_personas
+     *
+     * @param integer $numPersonas
+     */
+    public function setNumPersonas($numPersonas)
+    {
+        $this->num_personas = $numPersonas;
+    }
+
+    /**
+     * Get num_personas
+     *
+     * @return integer 
+     */
+    public function getNumPersonas()
+    {
+        return $this->num_personas;
+    }
+
+    /**
+     * Set ocupado
+     *
+     * @param boolean $ocupado
+     */
+    public function setOcupado($ocupado)
+    {
+        $this->ocupado = $ocupado;
+    }
+
+    /**
+     * Get ocupado
+     *
+     * @return boolean 
+     */
+    public function getOcupado()
+    {
+        return $this->ocupado;
+    }
+
+    /**
+     * Set via
+     *
+     * @param string $via
+     */
+    public function setVia($via)
+    {
+        $this->via = $via;
+    }
+
+    /**
+     * Get via
+     *
+     * @return string 
+     */
+    public function getVia()
+    {
+        return $this->via;
+    }
+    
+    /**
+     * Set nombre_via
+     *
+     * @param string $nombreVia
+     */
+    public function setNombreVia($nombreVia)
+    {
+        $this->nombre_via = $nombreVia;
+    }
+
+    /**
+     * Get nombre_via
+     *
+     * @return string 
+     */
+    public function getNombreVia()
+    {
+        return $this->nombre_via;
+    }
+
+    /**
+     * Set numero
+     *
+     * @param integer $numero
+     */
+    public function setNumero($numero)
+    {
+        $this->numero = $numero;
+    }
+
+    /**
+     * Get numero
+     *
+     * @return integer 
+     */
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    /**
+     * Set bloque
+     *
+     * @param integer $bloque
+     */
+    public function setBloque($bloque)
+    {
+        $this->bloque = $bloque;
+    }
+
+    /**
+     * Get bloque
+     *
+     * @return integer 
+     */
+    public function getBloque()
+    {
+        return $this->bloque;
+    }
+
+    /**
+     * Set puerta
+     *
+     * @param string $puerta
+     */
+    public function setPuerta($puerta)
+    {
+        $this->puerta = $puerta;
+    }
+
+    /**
+     * Get puerta
+     *
+     * @return string 
+     */
+    public function getPuerta()
+    {
+        return $this->puerta;
+    }
+
+    /**
+     * Set planta
+     *
+     * @param integer $planta
+     */
+    public function setPlanta($planta)
+    {
+        $this->planta = $planta;
+    }
+
+    /**
+     * Get planta
+     *
+     * @return integer 
+     */
+    public function getPlanta()
+    {
+        return $this->planta;
+    }
+
+    /**
+     * Set nombre_propietario
+     *
+     * @param string $nombrePropietario
+     */
+    public function setNombrePropietario($nombrePropietario)
+    {
+        $this->nombre_propietario = $nombrePropietario;
+    }
+
+    /**
+     * Get nombre_propietario
+     *
+     * @return string 
+     */
+    public function getNombrePropietario()
+    {
+        return $this->nombre_propietario;
+    }
+
+    /**
+     * Set habitaciones
+     *
+     * @param integer $habitaciones
+     */
+    public function setHabitaciones($habitaciones)
+    {
+        $this->habitaciones = $habitaciones;
+    }
+
+    /**
+     * Get habitaciones
+     *
+     * @return integer 
+     */
+    public function getHabitaciones()
+    {
+        return $this->habitaciones;
+    }
+
+    /**
+     * Set usuarios
+     *
+     * @param Vecinos\InmuebleBundle\Usuario $usuarios
+     */
+    public function setUsuarios($usuarios)
+    {
+        $this->usuarios = $usuarios;
+    }
+
+    /**
+     * Get usuarios
+     *
+     * @return Doctrine\Common\Collections\Collection $usuarios
+     */
+    public function getUsuarios()
+    {
+        return $this->usuarios;
+    }
+   
+
 }
--- src/Vecinos/UsuarioBundle/Entity/Usuario.php
+++ src/Vecinos/UsuarioBundle/Entity/Usuario.php
@@ -2,6 +2,7 @@
 
 namespace Vecinos\UsuarioBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -21,7 +22,93 @@ class Usuario
      */
     private $id;
 
+    /**
+     * @var string $nombre
+     *
+     * @ORM\Column(name="nombre", type="string", length=100)
+     */
+    private $nombre;
 
+    /**
+     * @var string $apellidos
+     *
+     * @ORM\Column(name="apellidos", type="string", length=255)
+     */
+    private $apellidos;
+
+    /**
+     * @var string $email
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string $password
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+    
+    /**
+     * @var string salt
+     *
+     * @ORM\Column(name="salt", type="string", length="255")
+     */
+    protected $salt;
+
+    /**
+     * @var text $direccion
+     *
+     * @ORM\Column(name="direccion", type="text")
+     */
+    private $direccion;
+    
+    /**
+     * @var boolean $permite_email
+     *
+     * @ORM\Column(name="permite_email", type="boolean")
+     */
+    private $permite_email;
+    
+    /**
+     * @var datetime $fecha_nacimiento
+     *
+     * @ORM\Column(name="fecha_nacimiento", type="datetime")
+     */
+    private $fecha_nacimiento;
+
+    /**
+     * @var string $dni
+     *
+     * @ORM\Column(name="dni", type="string", length=9)
+     */
+    private $dni;
+
+    /**
+     * @var string $ciudad
+     *
+     * @ORM\Column(name="ciudad", type="string", length=255)
+     */
+    private $ciudad;
+
+    /**
+     * 
+     *
+     * @ORM\ManyToMany(targetEntity="Inmueble", mappedBy="usuarios", cascade={"persist"})
+     */
+    private $inmuebles;
+
+    public function __construct()
+    {
+        $this->usuarios = new ArrayCollection();
+    }
+    
+    public function __toString()
+    {
+        return $this->getNombre().' '.$this->getApellidos();
+    }
+    
     /**
      * Get id
      *
@@ -31,4 +118,226 @@ class Usuario
     {
         return $this->id;
     }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+    
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set apellidos
+     *
+     * @param string $apellidos
+     */
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+    }
+
+    /**
+     * Get apellidos
+     *
+     * @return string 
+     */
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    
+    /**
+     * Set salt
+     *
+     * @param string $salt
+     */
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+    }
+
+    /**
+     * Get salt
+     *
+     * @return string 
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * Set direccion
+     *
+     * @param text $direccion
+     */
+    public function setDireccion($direccion)
+    {
+        $this->direccion = $direccion;
+    }
+
+    /**
+     * Get direccion
+     *
+     * @return text 
+     */
+    public function getDireccion()
+    {
+        return $this->direccion;
+    }
+
+    /**
+     * Set permite_email
+     *
+     * @param boolean $permiteEmail
+     */
+    public function setPermiteEmail($permiteEmail)
+    {
+        $this->permite_email = $permiteEmail;
+    }
+
+    /**
+     * Get permite_email
+     *
+     * @return boolean 
+     */
+    public function getPermiteEmail()
+    {
+        return $this->permite_email;
+    }
+    
+    /**
+     * Set fecha_nacimiento
+     *
+     * @param datetime $fechaNacimiento
+     */
+    public function setFechaNacimiento($fechaNacimiento)
+    {
+        $this->fecha_nacimiento = $fechaNacimiento;
+    }
+
+    /**
+     * Get fecha_nacimiento
+     *
+     * @return datetime 
+     */
+    public function getFechaNacimiento()
+    {
+        return $this->fecha_nacimiento;
+    }
+
+    /**
+     * Set dni
+     *
+     * @param string $dni
+     */
+    public function setDni($dni)
+    {
+        $this->dni = $dni;
+    }
+
+    /**
+     * Get dni
+     *
+     * @return string 
+     */
+    public function getDni()
+    {
+        return $this->dni;
+    }
+
+    /**
+     * Set ciudad
+     *
+     * @param string $ciudad
+     */
+    public function setCiudad($ciudad)
+    {
+        $this->ciudad = $ciudad;
+    }
+
+    /**
+     * Get ciudad
+     *
+     * @return string 
+     */
+    public function getCiudad()
+    {
+        return $this->ciudad;
+    }
+
+    /**
+     * Set inmuebles
+     *
+     * @param Vecinos\UsuarioBundle\Entity\Inmueble $inmuebles
+     */
+
+    public function setInmuebles($inmuebles)
+    {
+        $this->inmuebles = $inmuebles;
+    }
+
+    /**
+     * Get inmuebles
+     *
+     * @return Doctrine\Common\Collections\Collection $inmuebles
+     */
+    public function getInmuebles()
+    {
+        return $this->inmuebles;
+    }
+
 }
