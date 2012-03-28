06a59a9dd71b78c6038a7ae5c8c7873ae48edfd6
Antonio Jesus <ajesus.etsii@gmail.com>
2012-03-28 19:00:34 +0200

    Creadas Inmueble e Incidencia Bundle

---
+++ src/Vecinos/IncidenciaBundle/Admin/IncidenciaAdmin.php
@@ -0,0 +1,51 @@
+<?php
+namespace Vecinos\IncidenciaBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+
+class IncidenciaAdmin extends Admin
+{
+    
+    protected function configureListFields(ListMapper $mapper)
+    {
+        $mapper
+           ->addIdentifier('titulo', null, array('label' => 'Título'))
+           ->add('descripcion')
+           ->add('fecha')
+           ->add('hora')
+           ->addIdentifier('resuelta', null, array('label' => 'Resuelta'))
+        ;
+    }
+    
+    protected function configureDatagridFilters(DatagridMapper $mapper)
+    {
+        $mapper
+            ->add('titulo')
+            #->add('fecha')
+            #->add('hora')
+            ->add('resuelta')
+            ->add('descripcion')    
+        ;
+    }
+    
+    protected function configureFormFields(FormMapper $mapper)
+    {
+        $mapper
+            ->add('titulo')
+            ->add('descripcion')
+            ->add('fecha')
+            ->add('hora')
+            ->add('resuelta')
+        ;
+    }
+
+
+
+    
+}
+
+
+?>
---
+++ src/Vecinos/InmuebleBundle/Admin/InmuebleAdmin.php
@@ -0,0 +1,60 @@
+<?php
+namespace Vecinos\InmuebleBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+
+class InmuebleAdmin extends Admin
+{
+    
+    protected function configureListFields(ListMapper $mapper)
+    {
+        $mapper
+           ->add('num_personas')
+           ->add('via')
+           ->add('nombre_via')
+           ->add('numero')
+           ->add('bloque')
+           ->add('puerta')
+           ->add('planta')
+           ->add('nombre_propietario')
+           ->add('habitaciones')
+           ->add('ocupado')
+           ->add('usuarios')    
+        ;
+    }
+    
+    protected function configureDatagridFilters(DatagridMapper $mapper)
+    {
+        $mapper
+            ->add('nombre_via')
+            ->add('nombre_propietario')
+            ->add('ocupado')    
+        ;
+    }
+    
+    protected function configureFormFields(FormMapper $mapper)
+    {
+        $mapper
+           ->add('num_personas')
+           ->add('via')
+           ->add('nombre_via')
+           ->add('numero')
+           ->add('bloque')
+           ->add('puerta')
+           ->add('planta')
+           ->add('nombre_propietario')
+           ->add('habitaciones')
+           ->add('ocupado')
+        ;
+    }
+
+      
+
+    
+}
+
+
+?>
