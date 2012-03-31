89e1f346b453e8013c7a01c6e7efc41e38d11ea4
Antonio Jesus <ajesus.etsii@gmail.com>
2012-03-31 15:40:46 +0200

    Creada JuntaAdmin

---
+++ src/Vecinos/JuntaBundle/Admin/JuntaAdmin.php
@@ -0,0 +1,51 @@
+<?php
+namespace Vecinos\JuntaBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+
+class JuntaAdmin extends Admin
+{
+    
+    protected function configureListFields(ListMapper $mapper)
+    {
+        $mapper
+           ->addIdentifier('fecha', null, array('label' => 'Fecha'))
+           ->add('titulo')
+           ->add('descripcion')
+           ->add('hora')
+           ->add('duracion')
+           ->add('usuarios')    
+        ;
+    }
+    
+    protected function configureDatagridFilters(DatagridMapper $mapper)
+    {
+        $mapper
+            ->add('titulo')
+            ->add('descripcion')
+            ->add('duracion')    
+        ;
+    }
+    
+    protected function configureFormFields(FormMapper $mapper)
+    {
+        $mapper
+           ->add('fecha')
+           ->add('titulo')
+           ->add('descripcion')
+           ->add('hora')
+           ->add('duracion')
+           ->add('usuarios')    
+        ;
+    }
+
+      
+
+    
+}
+
+
+?>
