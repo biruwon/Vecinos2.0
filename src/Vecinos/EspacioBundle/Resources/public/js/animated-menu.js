c3d95b943bac15307e5a0099e5917a5cce6627de
Antonio Jesus <ajesus.etsii@gmail.com>
2012-05-18 18:40:17 +0200

    Arreglado errores js y css del menu

--- src/Vecinos/EspacioBundle/Resources/public/js/animated-menu.js
+++ src/Vecinos/EspacioBundle/Resources/public/js/animated-menu.js
@@ -1,12 +1,12 @@
     $(document).ready(function(){  
       
         //When mouse rolls over  
-        $("li").mouseover(function(){  
+        $("li.header1").mouseover(function(){  
             $(this).stop().animate({height:'150px'},{queue:false, duration:600, easing: 'easeOutBounce'})  
         });  
       
         //When mouse is removed  
-        $("li").mouseout(function(){  
+        $("li.header1").mouseout(function(){  
             $(this).stop().animate({height:'50px'},{queue:false, duration:600, easing: 'easeOutBounce'})  
         });  
       
